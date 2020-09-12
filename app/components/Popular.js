f9c933396266b833352c44636d63dabc59325e9b
Prem Puttegowda <puttegowdaprem@gmail.com>
2020-09-11 20:13:44 -0700

    added state to nav

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -2,23 +2,43 @@ import React from 'react';
 
 
 export default class Popular extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            selectedLanguage: 'All'
+        }
+        this.updateLanguage = this.updateLanguage.bind(this)
+    }
+
+    
+      updateLanguage (selectedLanguage) {
+        this.setState({
+          selectedLanguage
+        })
+      }
 
     render (){
+        
         const languages = ['All', 'python', 'Javascript', 'php']
         return (
+            
             <React.Fragment>
                 <nav className='sidebar'>
                     <ul className="sidebar-nav">
                         {languages.map((language)=> (
-                            <li key={language}className='sidebar-nav__item'>
-                                <a href="" className='sidebar-nav__link'>
+                            <li key={language}  className="sidebar-nav__item">
+                                <button
+                                    className={(language === this.state.selectedLanguage ? 'sidebar-nav__btn--active' : 'sidebar-nav__btn')}
+                                    onClick={() => this.updateLanguage(language)}>
                                     {language}
-                                </a>
+                                     </button>
+                             
                             </li>
                         ))}
                     </ul>
                 </nav>
             </React.Fragment>
+                
         )
     }
 }
